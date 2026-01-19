9d6611331a439398d58d82ab69e481366a99d4e4
Dark0deXMR <79117910+Dark0deXMR@users.noreply.github.com>
2026-01-19 14:11:22 +0100

    Update Dark0de1.c

--- Dark0de1.c
+++ Dark0de1.c
@@ -1,47 +1,34 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/wait.h>
 
-#define PORT "22"
 #define RATE "300000"
 #define OUTFILE "bios.txt"
 
 #define GREEN  "\033[92m"
-#define YELLOW "\033[93m"
 #define WHITE  "\033[97m"
 #define RESET  "\033[0m"
 
-pid_t progress_pid = 0;
+pid_t banner_pid = 0;
 
-void progress_loop(int x) {
-    for (int a = 0; a <= 255; a++) {
-        for (int b = 0; b <= 255; b++) {
-            printf(GREEN "[Portscan] - " WHITE "%d.%d.%d.*" RESET "\n", x, a, b);
-            fflush(stdout);
-            usleep(4000);
-        }
-    }
-}
-
-void stop_progress() {
-    if (progress_pid > 0) {
-        kill(progress_pid, SIGKILL);
-    }
+void usage(char *p) {
+    printf("Uso: %s <rango> -p<puertos>\n", p);
+    printf("Ejemplo:\n");
+    printf("  %s 114 -p22\n", p);
+    printf("  %s 114-120 -p22,2222,80\n", p);
+    exit(1);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Uso: %s <primer_octeto>\n", argv[0]);
-        return 1;
-    }
+    if (argc != 3 || strncmp(argv[2], "-p", 2) != 0)
+        usage(argv[0]);
 
-    int x = atoi(argv[1]);
-    if (x < 0 || x > 255) {
-        printf("Octeto inválido\n");
-        return 1;
-    }
+    char *range = argv[1];
+    char *ports = argv[2] + 2;
 
     FILE *out = fopen(OUTFILE, "w");
     if (!out) {
@@ -49,18 +36,22 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    signal(SIGINT, stop_progress);
+    int pipefd[2];
+    pipe(pipefd);
 
-    progress_pid = fork();
-    if (progress_pid == 0) {
-        progress_loop(x);
-        exit(0);
+    banner_pid = fork();
+    if (banner_pid == 0) {
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[1]);
+        execl("./banner", "banner", "-", ports, "2000", NULL);
+        exit(1);
     }
+    close(pipefd[0]);
 
     char cmd[512];
     snprintf(cmd, sizeof(cmd),
-        "masscan %d.0.0.0/8 -p%s --rate %s -oL -",
-        x, PORT, RATE
+        "masscan %s.0.0.0/8 -p%s --rate %s --wait 0 --quiet",
+        range, ports, RATE
     );
 
     FILE *fp = popen(cmd, "r");
@@ -69,28 +60,30 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char line[512];
+    char line[256];
     while (fgets(line, sizeof(line), fp)) {
-        if (line[0] == '#') continue;
+        char *p = strstr(line, "on ");
+        if (!p) continue;
 
-        char proto[8];
-        int port;
-        char status[16];
-        char ip[64];
+        p += 3;
+        char *e = strchr(p, '\n');
+        if (e) *e = 0;
 
-        if (sscanf(line, "%7s %d %15s %63s", proto, &port, status, ip) == 4) {
-            printf(YELLOW "%s" RESET "\n", ip);
-            fprintf(out, "%s\n", ip);
-            fflush(out);
-        }
+        fprintf(out, "%s\n", p);
+        fflush(out);
+
+        dprintf(pipefd[1], "%s\n", p);
+
+        printf(GREEN "[SCAN ] " WHITE "%s\n" RESET, p);
+        fflush(stdout);
     }
 
     pclose(fp);
     fclose(out);
-    stop_progress();
+    close(pipefd[1]);
 
-    printf(GREEN "[✔] Scan finalizado\n" RESET);
-    printf(GREEN "[✔] IPs guardadas en " WHITE "%s\n" RESET, OUTFILE);
+    wait(NULL);
 
+    printf(GREEN "[?] Scan finalizado\n" RESET);
     return 0;
 }
